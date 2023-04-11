2114bb7f6ff58918bb4deaf8964e8bc37fcc7059
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2023-04-11 17:14:55 +0100

    WIP

--- tests/Feature/FetchCommandTest.php
+++ tests/Feature/FetchCommandTest.php
@@ -5,6 +5,7 @@ use CodebarAg\LaravelTranslations\Commands\LaravelTranslationsFetchCommand;
 use Illuminate\Support\Facades\Storage;
 
 test('can fetch translations', function () {
+    Storage::fake();
 
     \Pest\Laravel\artisan(LaravelTranslationsFetchCommand::class, ['locale' => 'test'])
         ->expectsOutput('2 translations.')
