0845508eae0ec2ef24a45bb259ddcbf7ebad67db
RhysLees <RhysLees@users.noreply.github.com>
2023-04-26 17:54:43 +0000

    Fix styling

--- src/Commands/LaravelTranslationsFetchCommand.php
+++ src/Commands/LaravelTranslationsFetchCommand.php
@@ -14,14 +14,14 @@ class LaravelTranslationsFetchCommand extends Command
     public function handle(): int
     {
 
-       //check if locale.json exists allow --force
-       if (file_exists(base_path("lang/{$this->argument('locale')}.json")) && ! $this->option('force')) {
-           if (! $this->confirm('This will overwrite "lang/'.$this->argument('locale').'.json" Do you wish to continue?')) {
-               $this->warn('Command aborted');
+        //check if locale.json exists allow --force
+        if (file_exists(base_path("lang/{$this->argument('locale')}.json")) && ! $this->option('force')) {
+            if (! $this->confirm('This will overwrite "lang/'.$this->argument('locale').'.json" Do you wish to continue?')) {
+                $this->warn('Command aborted');
 
-               return Command::FAILURE;
-           }
-       }
+                return Command::FAILURE;
+            }
+        }
 
         $count = LaravelTranslationsFetch::handle($this->argument('locale'));
 
